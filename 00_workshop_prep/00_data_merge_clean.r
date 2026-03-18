0589c0174cdd4ef655f5a72e16a66700e0ca7a0a
Jeremy <jeremy.m.mikecz@dartmouth.edu>
2026-03-18 13:59:34 -0400

    simplify datasets

--- 00_workshop_prep/00_data_merge_clean.r
+++ 00_workshop_prep/00_data_merge_clean.r
@@ -133,9 +133,9 @@ movies_cleaned <- movies_cleaned |>
 
 ## write up the full clean data ---
   
-write.csv(movies_cleaned,"data/movie_data.csv", row.names = FALSE)
+# write.csv(movies_cleaned,"data/movie_data.csv", row.names = FALSE)
 
-colnames(movies_cleaned)
+# colnames(movies_cleaned)
 
 
 
@@ -175,7 +175,7 @@ movies_subset <- movies_cleaned |>
          description
          )
 
-write.csv(movies_subset,"data/movie_data_subset.csv", row.names = FALSE)
+# write.csv(movies_subset,"data/movie_data_subset.csv", row.names = FALSE)
 
 # FILTER DATASET - KEEP ONLY FILMS WITH "$" CURRENCY FOR 
 # BUDGET AND GROSS_WORLDWIDE
@@ -183,7 +183,7 @@ write.csv(movies_subset,"data/movie_data_subset.csv", row.names = FALSE)
 movies_subset_dollars <- movies_subset |>
   filter(budget_currency == "$" & grossww_currency == "$")
 
-write_csv(movies_subset_dollars, "data/movie_data_dollars.csv")
+write_csv(movies_subset_dollars, "data/movie_data.csv")
 
 # Additional EDA findings to clean during session? ----
 ## All variables but Year, Rating, and méta_score are chr
